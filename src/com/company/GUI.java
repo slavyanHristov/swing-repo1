b2f93ca81b70677364d457d6b9d92dffb6931efe
slavyanHristov <slavqnhristov@gmail.com>
2020-09-27 13:05:21 +0300

    My first Java Swing Project
    Button click counter

---
+++ src/com/company/GUI.java
@@ -0,0 +1,38 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUI implements ActionListener {
+    public int count = 0;
+    public JLabel label;
+    public JFrame frame;
+    public JPanel panel;
+    public JButton button;
+
+    public GUI() {
+         frame = new JFrame();
+         button = new JButton("Click");
+        button.addActionListener(this);
+        label = new JLabel("Number of clicks: " + count);
+         panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setLayout(new GridLayout(0, 1));
+        panel.add(button);
+        panel.add(label);
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Slavyan's GUI");
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        count++;
+        label.setText("Number of clicks: " + count);
+    }
+}
